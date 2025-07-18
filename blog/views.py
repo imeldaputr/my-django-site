967404db12129ccf0b0fa95a48fee12b298c2fff
imeldaputr <imel.kein.email@gmail.com>
2025-07-18 10:59:00 +0200

    set up admin page and database

--- blog/views.py
+++ blog/views.py
@@ -1,93 +1,26 @@
-from datetime import date
+from django.shortcuts import render, get_object_or_404
 
-from django.shortcuts import render
-
-all_posts = [
-    {
-        "slug": "my-first-post",
-        "image": "mountains.jpg",
-        "author": "Imelda Putri",
-        "date": date(2025, 7, 10),
-        "title": "My First Post",
-        "excerpt": "This is my first post. I am so excited to share my thoughts with you!",
-        "content": """
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore 
-            magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-            
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore 
-            magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-            
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore 
-            magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-        """
-    },
-    {
-        "slug": "programming-is-fun",
-        "image": "coding.jpg",
-        "author": "Maximilian",
-        "date": date(2022, 3, 10),
-        "title": "Programming Is Great!",
-        "excerpt": "Did you ever spend hours searching that one error in your code? Yep - that's what happened to me yesterday...",
-        "content": """
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-        """
-    },
-    {
-        "slug": "into-the-woods",
-        "image": "woods.jpg",
-        "author": "Maximilian",
-        "date": date(2020, 8, 5),
-        "title": "Nature At Its Best",
-        "excerpt": "Nature is amazing! The amount of inspiration I get when walking in nature is incredible!",
-        "content": """
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-        """
-    }
-]
-
-
-def get_date(post):
-    return post['date']
-
+from .models import Post
 
 # Create your views here.
 
 def starting_page(request):
-    sorted_posts = sorted(all_posts, key=get_date)
-    latest_posts = sorted_posts[-3:]  # Get the last 3 posts (latest posts)
+    latest_posts = Post.objects.all().order_by("-date")[:3]  # Fetch all posts from the database, ordered by date in descending order
     return render(request, "blog/index.html", { # Render the index.html template and return it as a view
         "posts": latest_posts
     }) 
 
 
 def posts(request):
+    all_posts = Post.objects.all().order_by("-date")  # Fetch all posts from the database, ordered by date in descending order
     return render(request, "blog/all-posts.html", {
         "all_posts": all_posts
     })
 
 
-def post_detail(request, slug):
-    identified_post = next(post for post in all_posts if post['slug'] == slug)  # Find the post with the given slug
+def post_detail(request, slug): # A single post detail view
+    identified_post = get_object_or_404(Post, slug=slug)  # Fetch the post with the given slug from the database
     return render(request, "blog/post-detail.html", {
-        "post": identified_post
+        "post": identified_post,
+        "post_tags": identified_post.tags.all()  # Fetch all tags associated with the post
     })
